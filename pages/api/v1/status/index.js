205163568221f85f12d61a22a4d1f9d5d9df13e3
Allan <allanfelipedk@gmail.com>
2026-03-05 15:50:12 -0300

    finish `api/v1/status` endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,8 +1,36 @@
-import database from "infra/database.js";
+import database from "infra/database";
+
 async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1 as sum");
-  console.log(result.rows);
-  response.status(200).json({ chave: "entrando em um protocolo HTTP" });
+  const updatedAt = new Date().toISOString();
+
+  const databaseVersionResult = await database.query("SHOW server_version;");
+  const databaseVersionValue = databaseVersionResult.rows[0].server_version;
+
+  const databaseMaxConnectionsResult = await database.query(
+    "SHOW max_connections;",
+  );
+  const databaseMaxConnectionsValue =
+    databaseMaxConnectionsResult.rows[0].max_connections;
+
+  const databaseName = process.env.POSTGRES_DB;
+
+  const databaseOpenConnectionsResult = await database.query({
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values: [databaseName],
+  });
+  const databaseOpenConnectionsValue =
+    databaseOpenConnectionsResult.rows[0].count;
+
+  response.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: databaseVersionValue,
+        max_connections: parseInt(databaseMaxConnectionsValue),
+        open_connections: databaseOpenConnectionsValue,
+      },
+    },
+  });
 }
 
 export default status;
