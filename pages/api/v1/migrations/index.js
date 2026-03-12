6f3ccfb6de3222f1e77172f9ba6cf97e30e1fd25
Allan <allanfelipedk@gmail.com>
2026-03-12 09:57:08 -0300

    add `api/v1/migrations` endpoint

---
+++ pages/api/v1/migrations/index.js
@@ -0,0 +1,39 @@
+import database from "infra/database";
+import migrationsRunner from "node-pg-migrate";
+import { join } from "node:path";
+
+export default async function migrations(request, response) {
+  const dbClient = await database.getNewClient();
+
+  const defaultMigrationsOptions = {
+    dbClient: dbClient,
+    dryRun: true,
+    dir: join("infra", "migrations"),
+    direction: "up",
+    verbose: true,
+    migrationsTable: "pgmigrations",
+  };
+
+  if (request.method === "GET") {
+    const pedingMigrations = await migrationsRunner(defaultMigrationsOptions);
+    await dbClient.end();
+    return response.status(200).json(pedingMigrations);
+  }
+
+  if (request.method === "POST") {
+    const migratedMigrations = await migrationsRunner({
+      ...defaultMigrationsOptions,
+      dryRun: false,
+    });
+
+    await dbClient.end();
+
+    if (migratedMigrations.length > 0) {
+      return response.status(201).json(migratedMigrations);
+    }
+
+    return response.status(200).json(migratedMigrations);
+  }
+
+  return response.status(405).end();
+}
